b9e30602c205c48bfddd6e5e15c5cb1f984abc97
abelLap <abeltrece@gmail.com>
2020-11-21 14:32:56 -0600

    Segunfo Sercidor

--- build/Controller/indexController.js
+++ build/Controller/indexController.js
@@ -1,16 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.indexController = void 0;
-const index_1 = require("../index");
 class IndexController {
     index(req, res) {
-        const io = require('socket.io')(index_1.server);
-        res.send("Index " + io.on('connection', (socket) => {
-            console.log("io's ready");
-            socket.on('Gola', (data) => {
-                console.log(data + "goo");
-            });
-        }));
+        res.send("Index ");
     }
 }
 exports.indexController = new IndexController();
--- src/Controller/indexController.ts
+++ src/Controller/indexController.ts
@@ -1,6 +1,6 @@
 import { Request, Response} from 'express';
 
-import { server } from '../index';
+
 import SocketIO, { Socket } from 'socket.io';
 
 class IndexController
@@ -8,19 +8,9 @@ class IndexController
 
     public index( req: Request, res:Response )
         {
-            const io = require('socket.io')(server);
+            
 
-            res.send("Index " + io.on('connection', (socket:Socket) => {
-                console.log("io's ready");
-                
-                socket.on('Gola',(data:any) =>
-                {
-                    console.log(data+"goo");
-                    
-                });    
-                
-              }));        
-
+            res.send("Index ");
 
         }
 }
